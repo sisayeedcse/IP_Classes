68b0663c9f18306f7325bf3f748e6d82f786fef0
sisayeedcse <118920944+sisayeedcse@users.noreply.github.com>
2024-01-09 11:04:22 +0600

    Add files via upload

---
+++ Class_07.js
@@ -0,0 +1,120 @@
+// Checking EVEN or ODD
+
+// var num = 10;
+// if(num%2==0)
+//     document.write("EVEN");
+// else
+//     document.write("ODD");
+
+//Checking Positive Or Negative
+// var num = prompt("Enter a number: ");
+// if(num > 0)
+//     document.write("Positive Number!");
+// else if (num < 0)
+//     document.write("Negative Number!");
+// else
+//     document.write("ZERO");
+
+// MARKSHEET GENERATOR
+
+// var mark = prompt("Enter your mark");
+// if(mark > 100){
+//     document.write("Please enter number 1 to 100!");
+// }
+// else if(mark <= 100 && mark >= 80){
+//     document.write(mark + " is " + "A+");
+// }
+// else if(mark <= 79 && mark >= 70){
+//     document.write(mark + " is " + "A");
+// }
+// else if(mark <= 69 && mark >= 55){
+//     document.write(mark + " is " + "A-");
+// }
+// else if(mark < 55 && mark >= 40){
+//     document.write(mark + " is " + "B");
+// }
+// else if(mark < 40 && mark >= 33){
+//     document.write(mark + " is " + "C");
+// }
+// else if(mark < 33){
+//     document.write(mark + " is " + "F");
+// }
+// else
+//     document.write("Invalid number ");
+
+// NUMBER GAME:
+
+// var num1 = parseInt(prompt("Enter number one: "));
+// var num2 = parseInt(prompt("Enter number two: "));
+// var num3 = parseInt(prompt("Enter number three: "));
+
+// if(num1 > num2 && num1 > num3){
+//     document.write(num1 + " is greater");
+// }
+// else if(num2 > num1 && num2 > num3){
+//     document.write(num2 + " is greater");
+// }
+// else if(num3 > num2 && num3 > num1){
+//     document.write(num3 + " is greater");
+// }
+
+//Checking Cowel or Consonent
+// var char = prompt("Enter a character! ");
+// char = char.toLowerCase();
+// if (char=='a' || char == 'e' || char == 'i' || char == 'o'|| char == 'u'){
+//     document.write("Vowel");
+// }
+// else document.write("Consonent!");
+
+
+// Printing digit value
+// var digit = parseInt(prompt("Enter a number: "))
+// switch(digit){
+//     case 0:
+//         document.write("ZERO");
+//         break;
+//     case 1:
+//         document.write("ONE");
+//         break;        
+//     case 2:
+//         document.write("TWO");
+//         break;        
+//     case 3:
+//         document.write("THREE"); 
+//         break;       
+//     case 4:
+//         document.write("FOUR");
+//         break;
+//     case 5:
+//         document.write("FIVE");
+//         break;
+//     case 6:
+//         document.write("SIX");
+//         break;
+//     case 7:
+//         document.write("SEVEN");
+//         break;
+//     case 8:
+//         document.write("EIGHT");
+//         break;
+//     case 9:
+//         document.write("NINE");
+//         break;
+//     default: document.write("Not a valid digit!");
+// }
+
+//VOWEL CONSONET USING SWITCH CASE
+// var charc =prompt("Enter an alphabet: ");
+// charc=charc.toLowerCase();
+
+// switch(charc){
+//     case 'a':
+//     case 'e':    
+//     case 'i':     
+//     case 'o':   
+//     case 'u':
+//         document.write("VOWEL");
+//         break;
+//     default: 
+//         document.write("Consonent!");
+// }
